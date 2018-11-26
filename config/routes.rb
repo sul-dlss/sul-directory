a5f625452301bec7958b596040380b99bd0722cf
Chris Beer <cabeer@stanford.edu>
2018-11-26 08:22:44 -0800

    Replace is_it_working with okcomputer; fixes #60

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :organizations
   root 'directory#index'
   get 'staff' => 'directory#index'
+  mount OkComputer::Engine, at: "/status"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
