c5060fcd9463d8ccc36542ad0a7835b9db8bfeb7
Chris Beer <cabeer@stanford.edu>
2020-03-27 17:08:02 -0700

    Tag migrations with a version

--- db/migrate/20150827075923_add_unique_index_to_organizations.rb
+++ db/migrate/20150827075923_add_unique_index_to_organizations.rb
@@ -1,4 +1,4 @@
-class AddUniqueIndexToOrganizations < ActiveRecord::Migration
+class AddUniqueIndexToOrganizations < ActiveRecord::Migration[5.2]
   def change
     add_index :organizations, :admin_id, unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150827075923) do
+ActiveRecord::Schema.define(version: 2015_08_27_075923) do
 
   create_table "organizations", force: :cascade do |t|
-    t.string   "admin_id"
-    t.string   "name"
-    t.integer  "parent_id"
+    t.string "admin_id"
+    t.string "name"
+    t.integer "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_organizations_on_admin_id", unique: true
   end
-
-  add_index "organizations", ["admin_id"], name: "index_organizations_on_admin_id", unique: true
 
 end
