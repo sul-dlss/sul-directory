02f791d2b336f0e4bdbc8f648f12b04db2986b57
Chris Beer <cabeer@stanford.edu>
2020-03-30 07:46:26 -0700

    Set the correct rails env for -dev

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,5 +1,5 @@
 server "sul-directory-dev.stanford.edu", user: fetch(:user), roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, "development"
+set :rails_env, "production"
 set :bundle_without, ['test']
